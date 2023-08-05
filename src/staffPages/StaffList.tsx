824bee7ed11ad8fa738800e5112c4396fb1a9011
Ong-Jun-Kai <ongjunkai@ecquaria.com>
2023-08-05 22:52:15 +0800

    Refactoring based on eslint

--- src/staffPages/StaffList.tsx
+++ src/staffPages/StaffList.tsx
@@ -1,23 +1,22 @@
-import { AppBar, Box, Button, Container, Grid, IconButton, Pagination, Paper, Toolbar, Typography } from '@mui/material'
-import axios from 'axios'
+import { Box, Button, Container, Grid, Pagination, Paper, Typography } from '@mui/material'
 import React, { useEffect, useState } from 'react'
 import CardsDisplay from './CardsDisplay'
 import TopNav from './TopNav'
-import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
-import * as Staffs from '../sampleData/Staffs.json';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline'
+import StaffsData from '../sampleData/Staffs.json'
 
 export default function ButtonAppBar () {
   const [loading, setLoading] = useState(false)
-  const [staffs, setStaffs] = useState<{name: string; gender: string; age: number; email: string;}[]>([])
+  const [staffs, setStaffs] = useState<Array<{ name: string, gender: string, age: number, email: string }>>([])
   const [currentPage, setCurrentPage] = useState(1)
   const [cardsPerPage] = useState(10)
 
   useEffect(() => {
     const getMovieDetails = async () => {
       setLoading(true)
-      const staffs = Staffs.staffs
+      const staffs = StaffsData.staffs
       setStaffs(staffs)
-        
+
       setLoading(false)
     }
 
@@ -25,9 +24,9 @@ export default function ButtonAppBar () {
   }, [currentPage])
 
   // Get currCards
-  /*const indexOfLastCard = currentPage * cardsPerPage
+  /* const indexOfLastCard = currentPage * cardsPerPage
   const indexOfFirstCard = indexOfLastCard - cardsPerPage
-  const currentCards = movieCard.slice(indexOfFirstCard, indexOfLastCard)*/
+  const currentCards = movieCard.slice(indexOfFirstCard, indexOfLastCard) */
 
   // Change page
   const paginate = (pageNumber: React.SetStateAction<number>) => { setCurrentPage(pageNumber) }
@@ -39,9 +38,9 @@ export default function ButtonAppBar () {
   return (
         <React.Fragment>
             <TopNav />
-            <Box sx={{ backgroundColor: '#EFF8FF', minHeight: '100vh'}}>
-                <Container sx={{pt:5, pb:5}}>
-                    <Paper elevation={0} sx={{p:5}}>
+            <Box sx={{ backgroundColor: '#EFF8FF', minHeight: '100vh' }}>
+                <Container sx={{ pt: 5, pb: 5 }}>
+                    <Paper elevation={0} sx={{ p: 5 }}>
                         <Grid container spacing={1} mb={2}>
                             <Grid xs={12} md={3}>
                                 <Typography variant="h5">Profiles</Typography>
@@ -50,8 +49,8 @@ export default function ButtonAppBar () {
 
                             </Grid>
                             <Grid xs={12} md={3}>
-                                <Button variant="contained" startIcon={<AddCircleOutlineIcon />}  size="large"
-                                        style={{backgroundColor: '#628DF2'}}
+                                <Button variant="contained" startIcon={<AddCircleOutlineIcon />} size="large"
+                                        style={{ backgroundColor: '#628DF2' }}
                                 >
                                     New Profile
                                 </Button>
